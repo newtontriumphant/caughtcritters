04fa1d2bb1e31f8c193ab0154f545db3ebbdb375
Newton Triumphant <funkeudo@outlook.com>
2021-01-20 00:35:04 +0000

    Added Swals

---
+++ script.js
@@ -0,0 +1,32 @@
+swal ({
+  title: "Welcome!",
+  text: "This is a simple test edition of our new website.",
+  icon: "r/buck3.svg",
+  timer: 10000,
+  })
+
+function img1() {
+  swal ({
+    title: "Two deer ruminating",
+    icon: "r/IMAG0047.JPG",
+    buttons: "Close",
+  });
+}
+
+
+function img2() {
+  swal ({
+    title: "A fat racccoon fleeing",
+    icon: "r/IMAG0061.JPG",
+    buttons: "Close",
+  });
+}
+
+
+function img3() {
+  swal ({
+    title: "A fawn coming from behind",
+    icon: "r/IMAG0123.JPG",
+    buttons: "Close",
+  });
+}
