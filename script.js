b70f5154318a4a7a0515c5f8a58cc64600e674ff
Newton Triumphant <funkeudo@outlook.com>
2021-03-11 03:22:54 +0000

    Changed to Lightboxes! Yay!

--- script.js
+++ script.js
@@ -1,32 +1,9 @@
+const el = document.createElement('div')
+el.innerHTML = "<a href='https://caughtcritters-1.mrnewton.repl.co/' target='blank'>https://caughtcritters-1.mrnewton.repl.co/</a>"
+
 swal ({
   title: "Welcome!",
-  text: "This is a simple test edition of our new website.",
+  text: "This is the second test edition of our new website. First edition: ",
   icon: "r/buck3.svg",
-  timer: 10000,
-  })
-
-function img1() {
-  swal ({
-    title: "Two deer ruminating",
-    icon: "r/IMAG0047.JPG",
-    buttons: "Close",
-  });
-}
-
-
-function img2() {
-  swal ({
-    title: "A fat racccoon fleeing",
-    icon: "r/IMAG0061.JPG",
-    buttons: "Close",
-  });
-}
-
-
-function img3() {
-  swal ({
-    title: "A fawn coming from behind",
-    icon: "r/IMAG0123.JPG",
-    buttons: "Close",
-  });
-}
+  content: el,
+})
